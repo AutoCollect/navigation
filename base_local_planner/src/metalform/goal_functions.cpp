9c5f39b3bf8b817be7cf0c08ec41b8e010d298d8
patrick <patrick.dong@metalform.co.nz>
2024-09-30 14:27:12 +1300

    short distance

--- base_local_planner/src/metalform/goal_functions.cpp
+++ base_local_planner/src/metalform/goal_functions.cpp
@@ -567,7 +567,7 @@ namespace base_local_planner {
         if ((min_dist < 0.5)  &&
             (has_suspect || (footprint_cost == costmap_2d::SUSPECT_OBSTACLE))) {
           // ROS_ERROR("[transformGlobalPlan] SUSPECT_OBSTACLE: reduce plan");
-          if (sq_dist <= 2.25) near_field_flag = true;
+          if (sq_dist <= 1.5) near_field_flag = true;
           break;
         }
         //=========================================
